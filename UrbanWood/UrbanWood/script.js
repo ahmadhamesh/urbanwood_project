95f6416c6d538c193ba5b77625db37f66f7e8f91
Ahmad Hamesh <110484706+ahmadhamesh@users.noreply.github.com>
2024-10-11 23:20:27 +0530

    Add files via upload

---
+++ UrbanWood/UrbanWood/script.js
@@ -0,0 +1,28 @@
+// $(document).ready(function(){
+//     $('.your-slider').slick({
+//         autoplay: true,
+//         autoplaySpeed: 2000,
+//         arrows: true,
+//         dots: true,
+//         infinite: true,
+//         speed: 500,
+//         slidesToShow: 1,
+//         slidesToScroll: 1
+//     });
+// });
+
+
+// const contentWrapper = document.getElementById('content');
+//   const scrollBtn = document.getElementById('scroll-btn');
+
+//   scrollBtn.addEventListener('click', function() {
+//     if (!contentWrapper.classList.contains('scrolled')) {
+//       contentWrapper.classList.add('scrolled');
+//       scrollBtn.textContent = "Scroll to Top";
+//     } else {
+//       contentWrapper.classList.remove('scrolled');
+//       contentWrapper.scrollTo({ top: 0, behavior: 'smooth' });
+//       scrollBtn.textContent = "Scroll to View More";
+//     }
+//   });
+
